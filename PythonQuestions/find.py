654a95a3bb54459d46dc08bbc57684ab7c5a0df3
andrewminai24 <andrewminai24@gmail.com>
2019-08-30 22:34:26 -0700

    find question

---
+++ PythonQuestions/find.py
@@ -0,0 +1,9 @@
+def find(nums):
+    max_num = float('-inf') # smaller tha
+    #other numbers
+    for nums in nums:
+            if num > max_num:
+                #fill in the missing line here
+        return max_num
+
+    print(max_num)
